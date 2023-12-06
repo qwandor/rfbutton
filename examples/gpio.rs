07126f5fb0de4c0276c03d24a471f7e5a05d8ba4
Andrew Walbran <qwandor@gmail.com>
2023-12-06 20:57:29 +0000

    Comment out settings which don't seem important.

--- examples/gpio.rs
+++ examples/gpio.rs
@@ -57,10 +57,10 @@ fn main() -> Result<(), Report> {
     cc1101.0.write_register(Config::IOCFG0, 0x0d).unwrap();
     // Disable data whitening and CRC, fixed packet length, asynchronous serial mode.
     cc1101.0.write_register(Config::PKTCTRL0, 0x30).unwrap();
-    cc1101.0.write_register(Config::PKTLEN, 0x04).unwrap();
+    //cc1101.0.write_register(Config::PKTLEN, 0x04).unwrap();
     // Frequency synthesizer offset (0x00 reset value).
     //cc1101.0.write_register(Config::FSCTRL0, 0x00).unwrap();
-    // Frequency synthesizer IF 211 kHz
+    // Frequency synthesizer IF 211 kHz. Doesn't seem to affect big button, but affects sensitivity to small remote.
     cc1101.0.write_register(Config::FSCTRL1, 0x06).unwrap();
     // Channel spacing. (Seems irrelevant, default value.)
     //cc1101.0.write_register(Config::MDMCFG0, 0xf8).unwrap();
@@ -75,29 +75,29 @@ fn main() -> Result<(), Report> {
     // Automatically calibrate when going from IDLE to RX or TX, XOSC stable timeout 64.
     cc1101.0.write_register(Config::MCSM0, 0x18).unwrap();
     // Clear channel indication always, RX off mode idle, TX off mode idle.
-    cc1101.0.write_register(Config::MCSM1, 0x00).unwrap();
+    //cc1101.0.write_register(Config::MCSM1, 0x00).unwrap();
     // RX timeout. (Seems irrelevant, default value.)
-    cc1101.0.write_register(Config::MCSM2, 0x07).unwrap();
-    // Medium hysteresis, 18 channel filter samples, normal operation, OOK decision boundary 12 dB
+    //cc1101.0.write_register(Config::MCSM2, 0x07).unwrap();
+    // Medium hysteresis, 18 channel filter samples, normal operation, OOK decision boundary 12 dB. Seems to affect sensitivity to small remote.
     cc1101.0.write_register(Config::AGCCTRL0, 0x92).unwrap();
-    // LNA2 gain decreased first, relavite carrier sense threshold disabled, absolute RSSI threshold at target setting.
-    cc1101.0.write_register(Config::AGCCTRL1, 0x00).unwrap();
+    // LNA2 gain decreased first, relative carrier sense threshold disabled, absolute RSSI threshold at target setting.
+    //cc1101.0.write_register(Config::AGCCTRL1, 0x00).unwrap();
     // All gain settings can be used, maximum possible LNA gain, 36 dB target value.
-    // TODO: 0x04 or 0x07?
+    // TODO: 0x04 or 0x07? 0x04 seems to let some noise through. Default value lets noise through all the time.
     cc1101.0.write_register(Config::AGCCTRL2, 0x07).unwrap();
     // Front-end TX current configuration.
-    cc1101.0.write_register(Config::FREND0, 0x11).unwrap();
-    // Front-end RX current configuration.
-    cc1101.0.write_register(Config::FREND1, 0xb6).unwrap();
+    //cc1101.0.write_register(Config::FREND0, 0x11).unwrap();
+    // Front-end RX current configuration. Unclear whether this affects sensitivity.
+    //cc1101.0.write_register(Config::FREND1, 0xb6).unwrap();
     // Frequency synthesiser calibration.
-    cc1101.0.write_register(Config::FSCAL0, 0x1f).unwrap();
+    /*cc1101.0.write_register(Config::FSCAL0, 0x1f).unwrap();
     cc1101.0.write_register(Config::FSCAL1, 0x00).unwrap();
     cc1101.0.write_register(Config::FSCAL2, 0x2a).unwrap();
-    cc1101.0.write_register(Config::FSCAL3, 0xe9).unwrap();
+    cc1101.0.write_register(Config::FSCAL3, 0xe9).unwrap();*/
     // Test settings.
-    cc1101.0.write_register(Config::TEST0, 0x09).unwrap();
+    /*cc1101.0.write_register(Config::TEST0, 0x09).unwrap();
     cc1101.0.write_register(Config::TEST1, 0x35).unwrap();
-    cc1101.0.write_register(Config::TEST2, 0x81).unwrap();
+    cc1101.0.write_register(Config::TEST2, 0x81).unwrap();*/
     cc1101.set_radio_mode(RadioMode::Receive).unwrap();
 
     println!("Set up CC1101, enabling interrupts...");
