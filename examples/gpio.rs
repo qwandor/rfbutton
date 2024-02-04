3f4ca69bc8864707875ba58593416f68d6f636eb
Andrew Walbran <qwandor@google.com>
2024-02-04 19:25:07 +0000

    Update for new embedded-hal.

--- examples/gpio.rs
+++ examples/gpio.rs
@@ -8,11 +8,13 @@ use cc1101::{
     lowlevel::types::AutoCalibration, Cc1101, FilterLength, Modulation, RadioMode, SyncMode,
     TargetAmplitude,
 };
-use eyre::{bail, Context, Report};
+use embedded_hal_bus::spi::ExclusiveDevice;
+use eyre::{bail, eyre, Context, Report};
 use log::{debug, trace};
 use rfbutton::decode;
 use rppal::{
     gpio::{Gpio, InputPin, Level, Trigger},
+    hal::Delay,
     spi::{Bus, Mode, SlaveSelect, Spi},
 };
 
@@ -32,37 +34,53 @@ fn main() -> Result<(), Report> {
     let mut rx_pin = gpio.get(RX_PIN)?.into_input();
 
     let cs = gpio.get(CS_PIN)?.into_output();
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode0)?;
-    let mut cc1101 = Cc1101::new(spi, cs).wrap_err("Error creating CC1101 device")?;
+    let spibus = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode0)?;
+    let spi = ExclusiveDevice::new(spibus, cs, Delay);
+    let mut cc1101 =
+        Cc1101::new(spi).map_err(|e| eyre!("Error creating CC1101 device: {:?}", e))?;
     cc1101.reset().unwrap();
     let (partnum, version) = cc1101
         .get_hw_info()
-        .wrap_err("Error getting hardware info")?;
+        .map_err(|e| eyre!("Error getting hardware info: {:?}", e))?;
     println!("Part number {}, version {}", partnum, version);
     cc1101
         .set_frequency(433940000)
-        .wrap_err("Error setting frequency")?;
-    cc1101.set_raw_mode()?;
+        .map_err(|e| eyre!("Error setting frequency: {:?}", e))?;
+    cc1101.set_raw_mode().map_err(|e| eyre!("{:?}", e))?;
 
     // Frequency synthesizer IF 211 kHz. Doesn't seem to affect big button, but affects sensitivity to small remote.
-    cc1101.set_synthesizer_if(152_300)?;
+    cc1101
+        .set_synthesizer_if(152_300)
+        .map_err(|e| eyre!("{:?}", e))?;
     // DC blocking filter enabled, OOK modulation, manchester encoding disabled, no preamble/sync.
-    cc1101.set_sync_mode(SyncMode::Disabled)?;
-    cc1101.set_modulation(Modulation::OnOffKeying)?;
+    cc1101
+        .set_sync_mode(SyncMode::Disabled)
+        .map_err(|e| eyre!("{:?}", e))?;
+    cc1101
+        .set_modulation(Modulation::OnOffKeying)
+        .map_err(|e| eyre!("{:?}", e))?;
     // Channel bandwidth and data rate.
-    cc1101.set_chanbw(232_000)?;
-    cc1101.set_data_rate(3_000)?;
+    cc1101.set_chanbw(232_000).map_err(|e| eyre!("{:?}", e))?;
+    cc1101.set_data_rate(3_000).map_err(|e| eyre!("{:?}", e))?;
     // Automatically calibrate when going from IDLE to RX or TX.
     // XOSC stable timeout was being set to 64, but this doesn't seem important.
-    cc1101.set_autocalibration(AutoCalibration::FromIdle)?;
+    cc1101
+        .set_autocalibration(AutoCalibration::FromIdle)
+        .map_err(|e| eyre!("{:?}", e))?;
     // Medium hysteresis, 16 channel filter samples, normal operation, OOK decision boundary 12 dB. Seems to affect sensitivity to small remote.
-    cc1101.set_agc_filter_length(FilterLength::Samples32)?;
+    cc1101
+        .set_agc_filter_length(FilterLength::Samples32)
+        .map_err(|e| eyre!("{:?}", e))?;
     // All gain settings can be used, maximum possible LNA gain, 36 dB target value.
     // TODO: 36 dB or 42 dB? 36 dB seems to let some noise through. Default value lets noise through all the time.
-    cc1101.set_agc_target(TargetAmplitude::Db42)?;
+    cc1101
+        .set_agc_target(TargetAmplitude::Db42)
+        .map_err(|e| eyre!("{:?}", e))?;
     // Front-end RX current configuration. Unclear whether this affects sensitivity.
     //cc1101.0.write_register(Config::FREND1, 0xb6)?;
-    cc1101.set_radio_mode(RadioMode::Receive)?;
+    cc1101
+        .set_radio_mode(RadioMode::Receive)
+        .map_err(|e| eyre!("{:?}", e))?;
 
     println!("Set up CC1101, enabling interrupts...");
 
